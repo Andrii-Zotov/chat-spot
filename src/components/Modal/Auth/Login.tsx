a378bd7455f3a053f06dcb527b816f6cad4ae1be
Andrii Zotov <andialzo@gmail.com>
2023-04-25 23:21:57 +0300

    Implemented Google auth and Sing up

--- src/components/Modal/Auth/Login.tsx
+++ src/components/Modal/Auth/Login.tsx
@@ -3,6 +3,9 @@ import { useState } from "react";
 import { Input, Button, border, Flex, Text } from "@chakra-ui/react";
 import { useSetRecoilState } from "recoil";
 import { authModalState } from "@/atoms/authModalAtom";
+import { useSignInWithEmailAndPassword } from "react-firebase-hooks/auth";
+import { auth } from "@/firebase/clientApp";
+import { FIREBASE_ERRORS } from "@/firebase/errors";
 
 type LoginProps = {};
 
@@ -12,8 +15,13 @@ const Login: React.FC<LoginProps> = () => {
     email: "",
     password: "",
   });
+  const [signInWithEmailAndPassword, user, loading, error] =
+    useSignInWithEmailAndPassword(auth);
   //Firebase logic
-  const onSubmit = () => {};
+  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    signInWithEmailAndPassword(loginForm.email, loginForm.password);
+  };
   const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     //Update form state
     setLoginForm((prev) => ({
@@ -59,7 +67,17 @@ const Login: React.FC<LoginProps> = () => {
         }}
         bg="gray.50"
       />
-      <Button width="100%" height="36px" mt={2} mb={2} type="submit">
+      <Text textAlign="center" color="red" fontSize="10pt">
+        {FIREBASE_ERRORS[error?.message as keyof typeof FIREBASE_ERRORS]}
+      </Text>
+      <Button
+        width="100%"
+        height="36px"
+        mt={2}
+        mb={2}
+        type="submit"
+        isLoading={loading}
+      >
         Log In
       </Button>
       <Flex fontSize="9pt" justifyContent="center">
--- src/components/Modal/Auth/OAuthButtons.tsx
+++ src/components/Modal/Auth/OAuthButtons.tsx
@@ -1,16 +1,25 @@
 import React from "react";
-import { Flex, Button, Image } from "@chakra-ui/react";
+import { Flex, Button, Image, Text } from "@chakra-ui/react";
+import { useSignInWithGoogle } from "react-firebase-hooks/auth";
+import { auth } from "@/firebase/clientApp";
 
 const OAuthButtons: React.FC = () => {
+  const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
   return (
     <Flex direction="column" width="100%" mb={4}>
-      <Button mb={2} variant="oauth">
+      <Button
+        mb={2}
+        variant="oauth"
+        isLoading={loading}
+        onClick={() => signInWithGoogle()}
+      >
         <Image height="30px" src="/images/googlelogo.png" mr={4} />
         Continue with Google
       </Button>
       <Button variant="oauth" mb={2}>
         Smth Else
       </Button>
+      {error && <Text>{error.message}</Text>}
     </Flex>
   );
 };
